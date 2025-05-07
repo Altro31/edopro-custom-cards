4c4aaed09f107bad8360f646b5127820e7c72a8f
Altro31 <albe020531@outlook.com>
2025-05-07 08:04:51 -0400

    transamu type fix

--- script/diskarma-ritual/c700000005.lua
+++ script/diskarma-ritual/c700000005.lua
@@ -1,27 +1,12 @@
 local s, id = GetID()
 function s.initial_effect(c)
-    --Activate
-    local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=aux.FilterBoolFunction(Card.IsCode,700000000),lv=11,extrafil=s.extragroup,
-									extraop=s.extraop,matfilter=s.matfilter,location=LOCATION_EXTRA,extratg=s.extratg})
+    local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,matfilter=s.forcedgroup})
 	c:RegisterEffect(e1)
 end
-
 s.listed_names = { 700000000 }
-
-function s.matfilter(c)
-	return c:IsFaceup() and c:IsAbleToGrave() and not c:IsMaximumModeSide()
+function s.ritualfil(c)
+	return c:IsCode(700000000)
 end
-
-function s.extragroup(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE,0,nil)
-end
-
-function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_MZONE)
-end
-
-function s.extraop(mat,e,tp,eg,ep,ev,re,r,rp,tc)
-    mat=mat:AddMaximumCheck()
-	Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+function s.forcedgroup(c,e,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup()
 end
